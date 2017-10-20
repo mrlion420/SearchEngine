134df412d0ebb71d6e963486057eb975d153b31c
Samuel Lin <samuel@aquametro-oil-marine.com.sg>
2017-10-20 02:53:53 +0000

    Initial Commit

---
+++ Crawler/Crawler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    public partial class Crawler : ServiceBase
+    {
+        public Crawler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string url = @"D:\Test\";
+            string[] filePaths = Directory.GetFiles(url, "*.txt");
+            foreach(string filePath in filePaths)
+            {
+                using (FileStream stream  = File.Open(filePath, FileMode.Open))
+                {
+                    
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
